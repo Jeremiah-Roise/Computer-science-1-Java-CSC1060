d52ac9ad579daa2fbca05eb3bf04b3e79e381610
Jeremiah <jeremiahroise@outlook.com>
2023-12-08 16:28:02 -0700

    finished class

--- test/Test.java
+++ test/Test.java
@@ -1,8 +1,12 @@
 public class Test {
-    public static void main(String[] args) {
-        System.out.println(1 + "A");
-        System.out.printf("%3.1f", 1234.56);
-        int i = 3434; double d = 3434;
-        System.out.printf("%5.1f %5.1f", i, d);
-    }
+int x;
+
+public Test(String t) {
+System.out.println("Test");
+}
+
+public static void main(String[] args) {
+Test test = new Test();
+System.out.println(test.x);
+}
 }
